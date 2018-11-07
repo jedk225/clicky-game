b93c1be008777655e45ffbf304a2a0229dbd377c
jedi225 <jedtk@uci.edu>
2018-11-06 19:50:49 -0800

    Updates

---
+++ precache-manifest.d4c28491133ba6d501be096043bb9dd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "e3dc6add2dd7c3872564",
+    "url": "/clicky-game/static/js/main.e3dc6add.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/clicky-game/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "e3dc6add2dd7c3872564",
+    "url": "/clicky-game/static/css/main.ff8a514a.chunk.css"
+  },
+  {
+    "revision": "3fe0be9546b8d4a86ee8cd7b1681b6db",
+    "url": "/clicky-game/index.html"
+  }
+];
